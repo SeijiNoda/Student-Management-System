3456093986aece97f5b24deb13a347c280a1f34c
Nícolas Duarte <nicolasmd9@hotmail.com>
2020-07-25 11:53:38 -0300

    update programa

--- Java/ManutencaoDeAlunos/src/Programa/Programa.java
+++ Java/ManutencaoDeAlunos/src/Programa/Programa.java
@@ -2,4 +2,17 @@ package Programa;
 
 public class Programa
 {
+    public static void main(String[] args)
+    {
+        try
+        {
+            Logradouro logradouro = (Logradouro) ClienteWS.getObjeto(Logradouro.class, "http://api.postmon.com.br/v1/cep/13098393");
+            System.out.println("\n\n");
+            System.out.println(logradouro);
+        }
+        catch(Exception erro)
+        {
+            System.err.println(erro.getMessage());
+        }
+    }
 }
