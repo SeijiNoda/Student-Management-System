dfb66d3cacac5e0816eb6e01933fb1cd7970c7bc
antoniokot <55291669+antoniokot@users.noreply.github.com>
2020-08-01 17:23:27 -0300

    Update index.js
    - Descomentei a minha string de conexão e fiz umas mudanças nos comandos sql

--- Node.js/index.js
+++ Node.js/index.js
@@ -3,9 +3,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 //STRING DO SEIJI
-const connStr = 'Server=regulus.cotuca.unicamp.br;Database=BD19190;User id=BD19190;Password=XADkhp4754';
+//const connStr = 'Server=regulus.cotuca.unicamp.br;Database=BD19190;User id=BD19190;Password=XADkhp4754';
 //STRING DO TONHAO
-// const connStr = 'Server=regulus.cotuca.unicamp.br;Database=BD19162;User id=BD19162;Password=frasemalucadotonhao';
+const connStr = 'Server=regulus.cotuca.unicamp.br;Database=BD19162;User id=BD19162;Password=felicidadeeounicocaminhoquevaleapena';
 //STRING DO NICOLINHO
 // const connStr = 'Server=regulus.cotuca.unicamp.br;Database=BD19192;User id=BD19192;Password=SENHADONICOLINHO';
 sql.connect(connStr)
@@ -29,10 +29,10 @@ const execSQLQuery = (command, res) => {
         .then(result => {
             //res.json(result.recordset);
             if(res.headersSent) return;
-            res.json({status: 'Requisição bem sucedida!'});
+            res.json({status: 'Requisição bem-sucedida!'});
         })
         .catch(err => {
-            res.json({status: `hi, there was an error: ${err}`})
+            res.json({status: `Requisição mal-sucedida: ${err}`})
             //res.send(500, {error: err});
         });
 };
@@ -43,6 +43,6 @@ router.post('/main/:ra/:cod/:nota/:freq', (req, res) => {
     const nota = parseFloat(req.params.nota);
     const frequencia = parseFloat(req.params.freq);
 
-    execSQLQuery(`INSERT INTO Resultados(RA,Cod,Nota,Frequencia) VALUES(${ra},${disciplina},${nota},${frequencia})`, res);
-    execSQLQuery(`DELETE FROM Matriculas WHERE RA = ${ra}`, res);
+    execSQLQuery(`INSERT INTO Resultado_ed(RA,Cod,Nota,Frequencia) VALUES(${ra},${disciplina},${nota},${frequencia})`, res);
+    execSQLQuery(`DELETE FROM Matricula_ed WHERE RA = ${ra} AND CODDISCIPLINA = ${disciplina}`, res);
 });
