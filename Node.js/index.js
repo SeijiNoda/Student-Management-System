6b05a47f60713265f4a6fbd77391f74ad137b1cc
SeijiNoda <49851925+SeijiNoda@users.noreply.github.com>
2020-08-08 09:05:22 -0300

    Final commit node

--- Node.js/index.js
+++ Node.js/index.js
@@ -2,12 +2,7 @@ const sql = require('mssql');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-//STRING DO SEIJI
-//const connStr = 'Server=regulus.cotuca.unicamp.br;Database=BD19190;User id=BD19190;Password=???';
-//STRING DO TONHAO
 const connStr = 'Server=regulus.cotuca.unicamp.br;Database=BD19162;User id=BD19162;Password=felicidadeeounicocaminhoquevaleapena';
-//STRING DO NICOLINHO
-// const connStr = 'Server=regulus.cotuca.unicamp.br;Database=BD19192;User id=BD19192;Password=SENHADONICOLINHO';
 sql.connect(connStr)
     .then(conn => global.conn = conn)
     .catch(err => console.log(err));
@@ -24,39 +19,23 @@ const port = 3000;
 app.listen(port);
 console.log("Active server");
 
-const execSQLQuery = (command, res) => {
-    global.conn.request().query(command)
+function execSQLQuery1(sqlQry, res) {
+    global.conn.request().query(sqlQry)
         .then(result => {
-            if(res.headersSent) return;
-            //res.json({status: 'Requisição bem-sucedida!'});
-            res.status(200).send(new String("SUCESSO"));
+            res.status(200).send(new String('Sucesso'))
+            console.log('Requisicao bem sucedida!');
         })
-        .catch(err => {
-            //res.json({status: `Requisição mal-sucedida: ${err}`})
-            if(res.headersSent) return;
-            //res.send(new String("OPERAÇÃO MAL-SUCEDIDA: "));
-            res.json(new String(`OPERAÇÃO MAL-SUCEDIDA: ${err}`));
+        .catch(err =>  {
+            res.json(new String('Operacao mal sucedida: ' + err))
+            console.log('Requisicao deu erro!');
         });
 };
 
-// router.get('/main/:ra/:cod/:nota/:freq', (req, res) => {
-//     const ra = parseInt(req.params.ra);
-//     const disciplina = parseInt(req.params.cod);
-//     const nota = parseFloat(req.params.nota);
-//     const frequencia = parseFloat(req.params.freq);
-
-//     execSQLQuery(`INSERT INTO Resultado_ed(RA,CodDisciplina,Nota,Frequencia) VALUES(${ra},${disciplina},${nota},${frequencia})`, res);
-//     execSQLQuery(`DELETE FROM Matricula_ed WHERE RA = ${ra} AND CODDISCIPLINA = ${disciplina}`, res);
-// });
 router.get('/main/:ra/:cod/:nota/:freq', (req, res) => {
     const ra = parseInt(req.params.ra);
     const disciplina = parseInt(req.params.cod);
     const nota = parseFloat(req.params.nota);
     const frequencia = parseFloat(req.params.freq);
 
-
-    execSQLQuery(`INSERT INTO Resultado_ed(RA,CodDisciplina,Nota,Frequencia) VALUES(${ra},${disciplina},${nota},${frequencia})`, res);
-    execSQLQuery(`DELETE FROM Matricula_ed WHERE RA = ${ra} AND CODDISCIPLINA = ${disciplina}`, res);
-
-    //res.send(new String("SUCESSO"));
+    execSQLQuery1(`INSERT INTO Resultado_ed(RA,CodDisciplina,Nota,Frequencia) VALUES(${ra},${disciplina},${nota},${frequencia})`, res);
 });
